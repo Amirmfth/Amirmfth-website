b9ba6be86fd5625bc7aae14e9adc69f8314a56b3
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-12-13 20:40:44 +0330

    added colors, fonts, screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
+    },
+    colors: {
+      "light-gray": "#F5F5F0",
+      "dark-gray": "#303030",
+    },
     extend: {},
   },
   plugins: [],
