dba4c7e6fdc1d8827e0fa47614fb5340b44022ce
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-12-15 17:43:56 +0330

    added fixed button to homepage

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,12 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Abilities from "./components/Abilities";
 import Hero from "./components/Hero";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 function HomePage() {
   return (
-    <main>
+    <div>
       <Hero />
       <Abilities />
-    </main>
+      <button className="fixed right-4 bottom-10 py-2 px-3 rounded-lg text-white bg-black md:right-10 dark:bg-white dark:text-black" onClick={e => window.scrollTo({top:0})}>
+        <FontAwesomeIcon icon={faArrowUp}/>
+        </button>
+    </div>
   );
 }
 
