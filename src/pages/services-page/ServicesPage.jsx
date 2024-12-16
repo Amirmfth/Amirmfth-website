dc929a1c31a43a722b59bea23e779cacad1a8a15
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-12-17 01:03:18 +0330

    added services section to services page

--- src/pages/services-page/ServicesPage.jsx
+++ src/pages/services-page/ServicesPage.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 function ServicesPage() {
   return (
     <div className="pt-32 md:pt-60">
       <Hero />
+      <Services />
     </div>
   );
 }
--- src/pages/services-page/components/Hero.jsx
+++ src/pages/services-page/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
         <h1 className="text-4xl font-semibold md:text-6xl">
           Building the Future, One Component at a Time
         </h1>
-        <p className="opacity-60">I'm passionate about crafting innovative and efficient web applications using React. With a keen eye for detail, I transform complex ideas into intuitive user experiences that delight and engage.</p>
+        <p className="opacity-60 text-lg">I'm passionate about crafting innovative and efficient web applications using React. With a keen eye for detail, I transform complex ideas into intuitive user experiences that delights and engage.</p>
       </div>
     </section>
   );
