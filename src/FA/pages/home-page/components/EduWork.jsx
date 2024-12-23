8a620afac61a9644cad93a511755f06461c5c1fe
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-12-23 21:07:18 +0330

    fixed duplicate lssues in hompage FA

--- src/FA/pages/home-page/components/EduWork.jsx
+++ src/FA/pages/home-page/components/EduWork.jsx
@@ -6,185 +6,94 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function EduWork() {
-  const lang = localStorage.getItem("lang");
   return (
-    <>
-      {lang === "en" ? (
-        <section id="edu-work" className="bg-light-gray dark:bg-dark-gray">
-          <div
-            data-aos="fade-up"
-            className="max-w-screen-lg flex flex-col pt-16 px-4 space-y-10 md:flex-row md:space-y-0 md:mx-auto "
-          >
-            {/* Education container */}
-            <div className="flex flex-col space-y-5 md:w-1/2 ">
-              {/* Header */}
-              <h2 className="text-3xl font-semibold tracking-wide">
-                Education history
-              </h2>
-              {/* Items */}
-              <div className="flex flex-col   divide-y divide-gray-500 divide-opacity-60 md:pr-10 ">
-                {/* Item 1 */}
-                <div data-aos="fade-up" className="flex space-x-5 py-8">
-                  {/* Logo */}
-                  <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
-                    <FontAwesomeIcon size="xl" icon={faGraduationCap} />
-                  </div>
-                  {/* description */}
-                  <div className="flex flex-col space-y-2">
-                    <h2 className="text-xl font-semibold">
-                      Shahid Beheshti's SAMPAD Highschool
-                    </h2>
-                    <p className="opacity-60">September 2017 - June 2021</p>
-                  </div>
-                </div>
-                {/* Item 2 */}
-                <div data-aos="fade-up" className="flex space-x-5 py-8">
-                  {/* Logo */}
-                  <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
-                    <FontAwesomeIcon size="xl" icon={faGraduationCap} />
-                  </div>
-                  {/* description */}
-                  <div className="flex flex-col space-y-2">
-                    <h2 className="text-xl font-semibold">
-                      Shahid Madani Highschool
-                    </h2>
-                    <p className="opacity-60">September 2021 - June 2022</p>
-                  </div>
-                </div>
-                {/* Item 3 */}
-                <div data-aos="fade-up" className="flex space-x-5 py-8">
-                  {/* Logo */}
-                  <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
-                    <FontAwesomeIcon size="xl" icon={faUserGraduate} />
-                  </div>
-                  {/* description */}
-                  <div className="flex flex-col space-y-2">
-                    <h2 className="text-xl font-semibold">
-                      Azad university S&R branch - Economics
-                    </h2>
-                    <p className="opacity-60">September 2022 - ...</p>
-                  </div>
-                </div>
+    <section id="edu-work" className="bg-light-gray dark:bg-dark-gray">
+      <div
+        data-aos="fade-up"
+        className="max-w-screen-lg flex flex-col pt-16 px-4 space-y-10 md:flex-row md:space-y-0 md:mx-auto "
+      >
+        {/* Education container */}
+        <div className="flex flex-col space-y-5 md:w-1/2 ">
+          {/* Header */}
+          <h2 className="text-3xl font-semibold tracking-wide">سابقه تحصیلی</h2>
+          {/* Items */}
+          <div className="flex flex-col divide-y divide-gray-500 divide-opacity-60 md:pl-10 ">
+            {/* Item 1 */}
+            <div
+              data-aos="fade-up"
+              className="flex space-x-5 space-x-reverse py-8"
+            >
+              {/* Logo */}
+              <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
+                <FontAwesomeIcon size="xl" icon={faGraduationCap} />
+              </div>
+              {/* description */}
+              <div className="flex flex-col space-y-2">
+                <h2 className="text-xl font-semibold">
+                  دبیرستان تیزهوشان شهید بهشتی
+                </h2>
+                <p className="opacity-60">مهر 1396 - خرداد 1401</p>
               </div>
             </div>
-            {/* Work container */}
-            <div className="flex flex-col space-y-5 md:w-1/2 ">
-              {/* Header */}
-              <h2 className="text-3xl font-semibold tracking-wide">
-                Working Experience
-              </h2>
-              {/* Items */}
-              <div className="flex flex-col  divide-y divide-gray-500 divide-opacity-60 ">
-                {/* Item 1 */}
-                <div data-aos="fade-up" className="flex space-x-5 py-8">
-                  {/* Logo */}
-                  <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
-                    <FontAwesomeIcon size="xl" icon={faBriefcase} />
-                  </div>
-                  {/* description */}
-                  <div className="flex flex-col space-y-2">
-                    <h2 className="text-xl font-semibold">
-                      Apprentice at MAPSA HR
-                    </h2>
-                    <p className="opacity-60">September 2022 - February 2023</p>
-                  </div>
-                </div>
+            {/* Item 2 */}
+            <div
+              data-aos="fade-up"
+              className="flex space-x-5 space-x-reverse py-8"
+            >
+              {/* Logo */}
+              <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
+                <FontAwesomeIcon size="xl" icon={faGraduationCap} />
+              </div>
+              {/* description */}
+              <div className="flex flex-col space-y-2">
+                <h2 className="text-xl font-semibold">دبیرستان شهید مدنی</h2>
+                <p className="opacity-60">مهر 1401 - خرداد 1402</p>
+              </div>
+            </div>
+            {/* Item 3 */}
+            <div
+              data-aos="fade-up"
+              className="flex space-x-5 space-x-reverse py-8"
+            >
+              {/* Logo */}
+              <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
+                <FontAwesomeIcon size="xl" icon={faUserGraduate} />
+              </div>
+              {/* description */}
+              <div className="flex flex-col space-y-2">
+                <h2 className="text-xl font-semibold">
+                  دانشگاه علوم و تحقیقات - اقتصاد
+                </h2>
+                <p className="opacity-60">مهر 1402 - ...</p>
               </div>
             </div>
           </div>
-        </section>
-      ) : (
-        <section id="edu-work" className="bg-light-gray dark:bg-dark-gray">
-          <div
-            data-aos="fade-up"
-            className="max-w-screen-lg flex flex-col pt-16 px-4 space-y-10 md:flex-row md:space-y-0 md:mx-auto "
-          >
-            {/* Education container */}
-            <div className="flex flex-col space-y-5 md:w-1/2 ">
-              {/* Header */}
-              <h2 className="text-3xl font-semibold tracking-wide">
-                سابقه تحصیلی
-              </h2>
-              {/* Items */}
-              <div className="flex flex-col divide-y divide-gray-500 divide-opacity-60 md:pl-10 ">
-                {/* Item 1 */}
-                <div
-                  data-aos="fade-up"
-                  className="flex space-x-5 space-x-reverse py-8"
-                >
-                  {/* Logo */}
-                  <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
-                    <FontAwesomeIcon size="xl" icon={faGraduationCap} />
-                  </div>
-                  {/* description */}
-                  <div className="flex flex-col space-y-2">
-                    <h2 className="text-xl font-semibold">
-                      دبیرستان تیزهوشان شهید بهشتی
-                    </h2>
-                    <p className="opacity-60">مهر 1396 - خرداد 1401</p>
-                  </div>
-                </div>
-                {/* Item 2 */}
-                <div
-                  data-aos="fade-up"
-                  className="flex space-x-5 space-x-reverse py-8"
-                >
-                  {/* Logo */}
-                  <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
-                    <FontAwesomeIcon size="xl" icon={faGraduationCap} />
-                  </div>
-                  {/* description */}
-                  <div className="flex flex-col space-y-2">
-                    <h2 className="text-xl font-semibold">
-                      دبیرستان شهید مدنی
-                    </h2>
-                    <p className="opacity-60">مهر 1401 - خرداد 1402</p>
-                  </div>
-                </div>
-                {/* Item 3 */}
-                <div data-aos="fade-up" className="flex space-x-5 space-x-reverse py-8">
-                  {/* Logo */}
-                  <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
-                    <FontAwesomeIcon size="xl" icon={faUserGraduate} />
-                  </div>
-                  {/* description */}
-                  <div className="flex flex-col space-y-2">
-                    <h2 className="text-xl font-semibold">
-                      دانشگاه علوم و تحقیقات - اقتصاد
-                    </h2>
-                    <p className="opacity-60">مهر 1402 - ...</p>
-                  </div>
-                </div>
+        </div>
+        {/* Work container */}
+        <div className="flex flex-col space-y-5 md:w-1/2 ">
+          {/* Header */}
+          <h2 className="text-3xl font-semibold tracking-wide">سابقه کاری</h2>
+          {/* Items */}
+          <div className="flex flex-col  divide-y divide-gray-500 divide-opacity-60 ">
+            {/* Item 1 */}
+            <div
+              data-aos="fade-up"
+              className="flex space-x-5 space-x-reverse py-8"
+            >
+              {/* Logo */}
+              <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
+                <FontAwesomeIcon size="xl" icon={faBriefcase} />
               </div>
-            </div>
-            {/* Work container */}
-            <div className="flex flex-col space-y-5 md:w-1/2 ">
-              {/* Header */}
-              <h2 className="text-3xl font-semibold tracking-wide">
-                سابقه کاری
-              </h2>
-              {/* Items */}
-              <div className="flex flex-col  divide-y divide-gray-500 divide-opacity-60 ">
-                {/* Item 1 */}
-                <div data-aos="fade-up" className="flex space-x-5 space-x-reverse py-8">
-                  {/* Logo */}
-                  <div className="flex h-fit my-auto items-center justify-center rounded-xl bg-white p-4 dark:bg-black ">
-                    <FontAwesomeIcon size="xl" icon={faBriefcase} />
-                  </div>
-                  {/* description */}
-                  <div className="flex flex-col space-y-2">
-                    <h2 className="text-xl font-semibold">
-                      کارآموز در MAPSA HR
-                    </h2>
-                    <p className="opacity-60">مهر 1401 - فروردین 1402</p>
-                  </div>
-                </div>
+              {/* description */}
+              <div className="flex flex-col space-y-2">
+                <h2 className="text-xl font-semibold">کارآموز در MAPSA HR</h2>
+                <p className="opacity-60">مهر 1401 - فروردین 1402</p>
               </div>
             </div>
           </div>
-        </section>
-      )}
-    </>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/FA/pages/home-page/components/Hero.jsx
+++ src/FA/pages/home-page/components/Hero.jsx
@@ -2,85 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Hero() {
-  const lang = localStorage.getItem("lang");
   return (
-    <>
-      {lang === "en" ? (
-        <section data-aos="fade-up" id="hero">
-          <div className="max-w-screen-lg flex flex-col mx-auto px-4 pb-10 space-y-10  md:flex-row md:items-start md:space-y-0 md:space-x-reverse  ">
-            {/* Name & profile Image */}
-            <div className="flex flex-col space-y-8">
-              {/* Profile Image */}
-              <img
-                src="/assets/images/profile-image.jpg"
-                alt="profile"
-                className="w-16 rounded-full"
-              />
-              {/* Name */}
-              <h1 className="text-3xl font-semibold text-right md:text-6xl">
-                Hello! I'm Amir.M.Fatahi
-              </h1>
-            </div>
-            {/* description & buttons */}
-            <div className="flex flex-col space-y-8">
-              {/* Description */}
-              <h2 className="text-2xl text-left md:text-5xl">
-                A React.js Developer based in Iran, Tehran.
-              </h2>
-              <p className="opacity-60 text-lg ">
-                Passionate creating SPA and fully functional websites
-              </p>
-              {/* Buttons */}
-              <div className="flex space-x-4">
-                <button className="px-4 py-2 text-white bg-black rounded-lg hover:opacity-70 hover:scale-105 dark:bg-white dark:text-black ">
-                  <Link to={"/en/about-me#contact-me"}>Talk with me</Link>
-                </button>
-                <button className="px-4 py-2 border border-black rounded-lg hover:opacity-70 hover:scale-105 dark:bg-black dark:text-white dark:border-white ">
-                  <Link to={"/en/about-me#contact-me"}>See my work</Link>
-                </button>
-              </div>
-            </div>
+    <section data-aos="fade-up" id="hero">
+      <div className="max-w-screen-lg flex flex-col mx-auto px-4 pb-10 space-y-10  md:flex-row md:items-start md:space-y-0 md:space-x-20 md:space-x-reverse  ">
+        {/* Name & profile Image */}
+        <div className="flex flex-col space-y-8">
+          {/* Profile Image */}
+          <img
+            src="/assets/images/profile-image.jpg"
+            alt="profile"
+            className="w-16 rounded-full"
+          />
+          {/* Name */}
+          <h1 className="text-3xl font-semibold text-right md:text-6xl">
+            سلام! من امیر محمد فتاحی هستم
+          </h1>
+        </div>
+        {/* description & buttons */}
+        <div className="flex flex-col space-y-8">
+          {/* Description */}
+          <h2 className="text-2xl text-right md:text-5xl">
+            برنامه نویس React.js واقع در تهران، ایران
+          </h2>
+          <p className="opacity-60 text-lg ">
+            مشتاقانه مشغول خلق وبسایت های تک صفحه ای و کاملا کاربردی
+          </p>
+          {/* Buttons */}
+          <div className="flex space-x-4 space-x-reverse">
+            <button className="px-4 py-2 text-white bg-black rounded-lg hover:opacity-70 hover:scale-105 dark:bg-white dark:text-black ">
+              <Link to={"/fa/about-me#contact-me"}>با من صحبت کن</Link>
+            </button>
+            <button className="px-4 py-2 border border-black rounded-lg hover:opacity-70 hover:scale-105 dark:bg-black dark:text-white dark:border-white ">
+              <Link to={"/fa/about-me#contact-me"}>مشاهده نمونه کارها</Link>
+            </button>
           </div>
-        </section>
-      ) : (
-        <section data-aos="fade-up" id="hero">
-          <div className="max-w-screen-lg flex flex-col mx-auto px-4 pb-10 space-y-10  md:flex-row md:items-start md:space-y-0 md:space-x-20 md:space-x-reverse  ">
-            {/* Name & profile Image */}
-            <div className="flex flex-col space-y-8">
-              {/* Profile Image */}
-              <img
-                src="/assets/images/profile-image.jpg"
-                alt="profile"
-                className="w-16 rounded-full"
-              />
-              {/* Name */}
-              <h1 className="text-3xl font-semibold text-right md:text-6xl">
-                سلام! من امیر محمد فتاحی هستم
-              </h1>
-            </div>
-            {/* description & buttons */}
-            <div className="flex flex-col space-y-8">
-              {/* Description */}
-              <h2 className="text-2xl text-right md:text-5xl">
-                برنامه نویس React.js واقع در تهران، ایران
-              </h2>
-              <p className="opacity-60 text-lg ">
-                مشتاقانه مشغول خلق وبسایت های تک صفحه ای و کاملا کاربردی
-              </p>
-              {/* Buttons */}
-              <div className="flex space-x-4 space-x-reverse">
-                <button className="px-4 py-2 text-white bg-black rounded-lg hover:opacity-70 hover:scale-105 dark:bg-white dark:text-black ">
-                  <Link to={"/fa/about-me#contact-me"}>با من صحبت کن</Link>
-                </button>
-                <button className="px-4 py-2 border border-black rounded-lg hover:opacity-70 hover:scale-105 dark:bg-black dark:text-white dark:border-white ">
-                  <Link to={"/fa/about-me#contact-me"}>مشاهده نمونه کارها</Link>
-                </button>
-              </div>
-            </div>
-          </div>
-        </section>
-      )}
-    </>
+        </div>
+      </div>
+    </section>
   );
 }
 
